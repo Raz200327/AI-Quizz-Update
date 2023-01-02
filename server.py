b394f91ee11f0c349dd46ad501214f6204ff7fe3
Raz200327 <115984448+Raz200327@users.noreply.github.com>
2023-01-02 12:50:18 +1100

    Update server.py

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizzes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config["CELERY_BROKER_URL"] = 'amqp://localhost//'
+app.config["CELERY_BROKER_URL"] = 'amqps://puavmzjw:Ehmm76DvNOQy1-nQqyOtv6ErMCeOQDIw@chimpanzee.rmq.cloudamqp.com/puavmzjw'
 app.config["CELERY_RESULT_BACKEND"] = "db+sqlite:///ryan.sqlite3"
 app.config["CELERYD_CONCURRENCY"] = 1
 
